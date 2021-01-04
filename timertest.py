eabac271de7c9e537c3dc87ea10d9ec75df16b30
adipotnis <50766264+adipotnis@users.noreply.github.com>
2021-01-04 18:59:13 +0530

    Create timertest.py

---
+++ timertest.py
@@ -0,0 +1,39 @@
+# import the necessary packages
+from threading import Thread
+import time
+class pingtimer:
+    def __init__(self, src=0, name="timer"):
+
+        # initialize the thread name
+        self.name = name
+        self.duration = 0
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+
+    def start(self, duration = 0):
+        # start the thread to read frames from the video stream
+        self.duration = duration
+        t = Thread(target=self.update, name=self.name, args=())
+        t.daemon = True
+        t.start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            time.sleep(self.duration)
+            print(1)
+            if self.stopped:
+                print('thread exit successfully')
+                return
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+
+t = pingtimer()
+t.start(duration=2)
+time.sleep(10)
+t.stop()
