f9a60ddb10aabe414f020a06de7d182999b7c7bb
adipotnis <50766264+adipotnis@users.noreply.github.com>
2021-01-04 18:00:56 +0530

    Add files via upload

--- facebound.py
+++ facebound.py
@@ -8,9 +8,7 @@ import time
 def centrelocator(x, y, w, h):
     return int((2*x + w)/2), int((2*y +h)/2)
 
-
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-
 
 mydrone = tello.Tello()
 mydrone.send_command('streamon')
@@ -18,13 +16,16 @@ cap = imutils.video.WebcamVideoStream(src="udp://@0.0.0.0:11111").start()   # th
 #cap = cv2.VideoCapture(0)
 mydrone.takeoff()
 
+centre = centrelocator(0, 0, (cap.read()).shape[1],(cap.read()).shape[0])
+
 while True:
     img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        cv2.circle(img, centrelocator(x, y, w, h), 9, (0, 255, 255), 18)
+        cv2.circle(img, centre,8,(0, 255, 255), 18 )        # screen centre
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)      # face ROI
+        cv2.circle(img, centrelocator(x, y, w, h), 9, (0, 255, 255), 18)        #  centre of ROI
         roi_gray = gray[y : y + h, x : x + w]
         roi_color = img[y : y + h, x : x + w]
     time.sleep(0.3)     # needs to be threaded parallely to avoid shutdown
