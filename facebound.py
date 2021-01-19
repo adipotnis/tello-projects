2d87ef28d4e3d9b16cc7fece9f154d04531492e8
adipotnis <50766264+adipotnis@users.noreply.github.com>
2021-01-19 18:02:23 +0530

    Update facebound.py

--- facebound.py
+++ facebound.py
@@ -1,9 +1,10 @@
 import cv2
+import time
 import numpy as np
 import imutils
 from imutils.video import WebcamVideoStream
 from easytello import tello
-import time
+from timertest import pingtimer
 
 def centrelocator(x, y, w, h):
     return int((2*x + w)/2), int((2*y +h)/2)
@@ -16,21 +17,24 @@ cap = imutils.video.WebcamVideoStream(src="udp://@0.0.0.0:11111").start()   # th
 #cap = cv2.VideoCapture(0)
 mydrone.takeoff()
 
-centre = centrelocator(0, 0, (cap.read()).shape[1],(cap.read()).shape[0])
+disp_centre = centrelocator(0, 0, (cap.read()).shape[1],(cap.read()).shape[0])
 
 while True:
     img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
-        cv2.circle(img, centre,8,(0, 255, 255), 18 )        # screen centre
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)      # face ROI
-        cv2.circle(img, centrelocator(x, y, w, h), 9, (0, 255, 255), 18)        #  centre of ROI
         roi_gray = gray[y : y + h, x : x + w]
         roi_color = img[y : y + h, x : x + w]
-    time.sleep(0.3)     # needs to be threaded parallely to avoid shutdown
-    mydrone.get_battery()
+        cv2.circle(img, disp_centre,8,(0, 255, 255), 18)        # screen centre
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)      # face ROI
+        face_centre = centrelocator(x, y, w, h)
+        cv2.circle(img, face_centre, 9, (0, 255, 255), 18)        #  centre of ROI
+        cv2.line(img, disp_centre, face_centre, (0, 255, 255), 3)
+
     cv2.imshow('img', img)
+
+
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
